9f85be572a9275602f5de3e1c825463ec57488ca
hoanghuybu <dohoanghuy8a3@gmail.com>
2023-03-27 20:10:03 +0700

    Read data from MongoDB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,8 +1,31 @@
+const Course = require('../models/Course');
+const { mutipleMongooseToObject } = require('../../util/mongoose');
+
 class SiteController {
     // [GET] /
-    index(req, res) {
-        res.render('home');
+    // -- Code 1 tương đương code 2
+    index(req, res, next) {
+        Course.find({})
+            .then((courses) => {
+                res.render('home', {
+                    courses: mutipleMongooseToObject(courses),
+                });
+            })
+            .catch((err) => next(err));
     }
+
+    // code 2
+    /*
+    async index(req, res, next) {
+        try {
+            const courses = await Course.find({});
+            res.json(courses);
+        } catch (err) {
+            console.error(err);
+            res.status(400).json({ error: 'Error' });
+        }
+    }
+    */
 
     // [GET] /search
     search(req, res) {
