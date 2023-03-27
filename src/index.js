4ea737e3c0c5e2391a50fc3ccb408fdd197315c1
hoanghuybu <dohoanghuy8a3@gmail.com>
2023-03-27 10:58:14 +0700

    v5

--- src/index.js
+++ src/index.js
@@ -13,22 +13,20 @@ app.use(morgan('combined'));
 
 //Templet engine
 app.engine(
-    '.hbs'    ,
 
-
-
+    
+    '.hbs',
 
     handlebars({
         extname: '.hbs',
     }),
 );
-                    app.set('view engine', '.hbs');
-                    app.
-                    set('views', path.join(__dirname, 'resource/views'));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resource/views'));
 
 // Routes init
 route(app);
 
-app.listen(port, () =>
-   console.log(`Example app listening at http://localhost:${port}`),
-);
+                    app.listen(port, () =>
+                        console.log(`Example app listening at http://localhost:${port}`),
+                    );
